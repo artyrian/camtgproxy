7405e9a89bca964f127ad024a853e592fb12121c
Artur Rakhmatullin <artyrian@yandex-team.ru>
2021-10-13 19:47:47 +0300

    add ip to log

--- src/rest_handlers.py
+++ src/rest_handlers.py
@@ -4,8 +4,12 @@ from aiohttp import web
 
 from . import cam_client
 
+
 async def nvrtotg(request):
+    ip = request.remote
     params = request.rel_url.query
+
+
 
     chat_id = request.app['chat_id']
     tg = request.app['tg']
@@ -15,7 +19,7 @@ async def nvrtotg(request):
     port = params.get('port', 9786)
     host = params.get('host')
     text = params.get('text')
-    logging.info('get request to proxy cam %s %s %s with text: %s', host, port, cam, text)
+    logging.info('[ip: %s] get request to proxy cam %s %s %s with text: %s', ip, host, port, cam, text)
 
     photo = cam_client.get_image(host=host, port=port, cam=cam, auth=cam_auth)
     tg.send_photo(chat_id, photo, caption=text)
